d2cccda58683ac290fe10471c6f70c00f677387f
BuildTools <unconfigured@null.spigotmc.org>
2021-07-08 04:08:36 -0500

    first commit

---
+++ source/Caching.hx
@@ -0,0 +1,125 @@
+package;
+
+import haxe.Exception;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import sys.FileSystem;
+import sys.io.File;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.transition.FlxTransitionSprite.GraphicTransTileDiamond;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.addons.transition.TransitionData;
+import flixel.graphics.FlxGraphic;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.math.FlxPoint;
+import flixel.math.FlxRect;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import flixel.text.FlxText;
+
+using StringTools;
+
+class Caching extends MusicBeatState
+{
+    var toBeDone = 0;
+    var done = 0;
+
+    var text:FlxText;
+    var kadeLogo:FlxSprite;
+
+	override function create()
+	{
+        FlxG.mouse.visible = false;
+
+        FlxG.worldBounds.set(0,0);
+
+        text = new FlxText(FlxG.width / 2, FlxG.height,0,"Loading...");
+        text.size = 34;
+        text.alignment = FlxTextAlign.CENTER;
+
+        kadeLogo = new FlxSprite(FlxG.width / 2, FlxG.height / 2).loadGraphic(Paths.image('FlippyLogo'));
+        kadeLogo.screenCenter(X);
+        kadeLogo.y = FlxG.height - kadeLogo.height;
+        text.y = FlxG.height - text.height - 20;
+        text.screenCenter(X);
+        text.x -= 50;
+        kadeLogo.setGraphicSize(Std.int(kadeLogo.width * 0.6));
+        kadeLogo.antialiasing = true;
+
+        add(kadeLogo);
+        add(text);
+
+
+        trace('starting caching..');
+        
+        sys.thread.Thread.create(() -> {
+            cache();
+        });
+
+
+        super.create();
+    }
+
+    var calledDone = false;
+
+    override function update(elapsed) 
+    {
+
+        if (toBeDone != 0 && done != toBeDone)
+        {
+            text.text = "Loading... (" + done + "/" + toBeDone + ")";
+        }
+
+        super.update(elapsed);
+    }
+
+
+    function cache()
+    {
+
+        var images = [];
+        var music = [];
+
+        trace("caching images...");
+
+        for (i in FileSystem.readDirectory(FileSystem.absolutePath("assets/shared/images/characters")))
+        {
+            if (!i.endsWith(".png"))
+                continue;
+            images.push(i);
+        }
+
+        trace("caching music...");
+
+        for (i in FileSystem.readDirectory(FileSystem.absolutePath("assets/songs")))
+        {
+            music.push(i);
+        }
+
+        toBeDone = Lambda.count(images) + Lambda.count(music);
+
+        trace("LOADING: " + toBeDone + " OBJECTS.");
+
+        for (i in images)
+        {
+            var replaced = i.replace(".png","");
+            FlxG.bitmap.add(Paths.image("characters/" + replaced,"shared"));
+            trace("cached " + replaced);
+            done++;
+        }
+
+        for (i in music)
+        {
+            FlxG.sound.cache(Paths.inst(i));
+            FlxG.sound.cache(Paths.voices(i));
+            trace("cached " + i);
+            done++;
+        }
+
+        trace("Finished caching...");
+
+        FlxG.switchState(new TitleState());
+    }
+
+}
---
+++ source/HealthIcon.hx
@@ -0,0 +1,43 @@
+package;
+
+import flixel.FlxSprite;
+
+class HealthIcon extends FlxSprite
+{
+	/**
+	 * Used for FreeplayState! If you use it elsewhere, prob gonna annoying
+	 */
+	public var sprTracker:FlxSprite;
+
+	public function new(char:String = 'bf', isPlayer:Bool = false)
+	{
+		super();
+		
+		loadGraphic(Paths.image('iconGrid'), true, 150, 150);
+
+		antialiasing = true;
+		animation.add('bf', [0, 1], 0, false, isPlayer);
+		animation.add('flippy', [24, 24], 0, false, isPlayer);
+		animation.add('flippy-crazy', [24, 24], 0, false, isPlayer);
+		animation.add('dad', [12, 13], 0, false, isPlayer);
+		animation.add('bf-old', [14, 15], 0, false, isPlayer);
+		animation.add('gf', [16], 0, false, isPlayer);
+		animation.play(char);
+
+		switch(char)
+		{
+			case 'bf-pixel' | 'senpai' | 'senpai-angry' | 'spirit' | 'gf-pixel':
+				antialiasing = false;
+		}
+
+		scrollFactor.set();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (sprTracker != null)
+			setPosition(sprTracker.x + sprTracker.width + 10, sprTracker.y - 30);
+	}
+}
